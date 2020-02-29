d68f153382ebdfcdeaba148a36e73632922d1a34
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-02-29 18:49:10 -0300

    validations for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ApplicationRecord
   belongs_to :category
 
   has_paper_trail
+
+  validates :price, numericality: { greater_than: 10 }
+  validates_presence_of :title, :code, :bar_code, :manufacturer, :model
+
+  validate :validity_of_bar_code
+
+  def validity_of_bar_code
+    errors.add(:base, 'Invalid bar code') unless Barkick::GTIN.new(bar_code).valid?
+  end
 end
