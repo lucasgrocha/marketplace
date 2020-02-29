e3b972ed190f5a485770f324deb50a54c3c4523f
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-02-29 17:40:19 -0300

    factory for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,25 @@
 FactoryBot.define do
   factory :user do
-    
+    email                 { FFaker::Internet.email }
+    first_name            { FFaker::Name.first_name }
+    last_name             { FFaker::Name.last_name }
+    gender                { FFaker::Gender.sample }
+    phone                 { FFaker::PhoneNumber.short_phone_number }
+    timezone              { FFaker::Address.time_zone }
+    birth_date            { FFaker::Time.date }
+    password              { 123456 }
+    password_confirmation { 123456 }
+
+    trait :admin do
+      after(:create) { |user| user.add_role(:admin) }
+    end
+
+    trait :customer do
+      after(:create) { |user| user.add_role(:customer) }
+    end
+
+    trait :manager do
+      after(:create) { |user| user.add_role(:manager) }
+    end
   end
 end
