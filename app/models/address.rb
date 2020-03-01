2eeb717ad9b5ae8696133e62353f7885e1ffc06b
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-03-01 13:45:36 -0300

    model test - address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user
 
+  validates :building_number, numericality: { greater_than: 0 }
   has_paper_trail
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates an address' do
+    expect {
+      create(:address)
+    }.to change(Address, :count)
+  end
+
+  it 'does not create an address with invalid building number' do
+    expect { create(:address, building_number: 0) }.to raise_error ActiveRecord::RecordInvalid
+  end
 end
