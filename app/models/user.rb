7a665d71a45abbf954c334e14d10ecec8c20023f
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-02-29 22:48:15 -0300

    isolate user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,11 @@
 class User < ApplicationRecord
+  prepend UserMethods
+
   rolify
+  has_paper_trail
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  has_paper_trail
-
-  def admin?
-    self.has_role? :admin
-  end
-
-  def customer?
-    self.has_role? :customer
-  end
-
-  def manager?
-    self.has_role? :manager
-  end
-
-  def full_name
-    "#{first_name} #{last_name}"
-  end
-
-  def under_age?
-    TimeDifference.between(DateTime.now.utc, birth_date).in_years <= 18
-  end
+          :recoverable, :rememberable, :validatable
 end
---
+++ app/models/user_methods.rb
@@ -0,0 +1,21 @@
+module UserMethods
+  def admin?
+    self.has_role? :admin
+  end
+
+  def customer?
+    self.has_role? :customer
+  end
+
+  def manager?
+    self.has_role? :manager
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def under_age?
+    TimeDifference.between(DateTime.now.utc, birth_date).in_years <= 18
+  end
+end
