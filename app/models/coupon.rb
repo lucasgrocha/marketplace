b3a29c61e6ca17cb405982d65cbb89cce6f09575
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-03-06 21:49:22 -0300

    service - apply coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -7,36 +7,4 @@ class Coupon < ApplicationRecord
   def expired?
     expiration_date <= DateTime.now.utc
   end
-
-  def self.apply_discount(product, coupon)
-    @coupon  = find(coupon)
-    @product = product
-    check_validity
-    check_coupon_for_category
-    do_apply
-  rescue => e
-    errors = ActiveModel::Errors.new(self)
-    errors.add(:base, e.message)
-  end
-
-  private
-
-  def self.check_validity
-    return true unless @coupon.expired?
-
-    raise 'Expired Coupon'
-  end
-
-  def self.check_coupon_for_category
-    return true if @product.category.coupons.pluck(:code).include? @coupon.code
-
-    raise 'Invalid Coupon for this category'
-  end
-
-  def self.do_apply
-    price = @product.price
-
-    return price - @coupon.discount_amount if @coupon.discount_type == 'fixed'
-    price - ( price * @coupon.discount_amount / 100 ).round(2)
-  end
 end
---
+++ app/services/apply_coupon_service.rb
@@ -0,0 +1,33 @@
+class ApplyCouponService
+  attr_reader :product, :coupon
+
+  def initialize(product_id, coupon_code)
+    @product = Product.find(product_id)
+    @coupon  = Coupon.find(coupon_code)
+  end
+
+  def perform
+    return 'Invalid Coupon' if invalid_coupon?
+    calculate_discount
+  end
+
+  private
+
+  def invalid_coupon?
+    coupon.expired? || !available_for_category?
+  end
+
+  def available_for_category?
+    product.category.coupons.ids.include? coupon.code
+  end
+
+  def calculate_discount
+    price           = product.price
+    discount_amount = coupon.discount_amount
+
+    return price - discount_amount if coupon.fixed?
+    return (price - ( price * discount_amount / 100 )).round(2) if coupon.percentage?
+
+    0
+  end
+end
