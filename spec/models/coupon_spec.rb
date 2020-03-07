99bc3754dc950c56445a61ea924ea21c33a7753a
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-03-07 12:24:49 -0300

    model test - coupon

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Coupon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when has invalid type' do
+    it 'raises error on creating' do
+      expect { build_stubbed(:coupon, discount_type: 'abc') }.to raise_error ArgumentError
+    end
+  end
+
+  describe '#expired?' do
+    subject(:coupon) { build_stubbed(:coupon) }
+
+    context 'when expiration_date is in the past' do
+      it 'returns true' do
+        coupon.expiration_date = DateTime.now - 1.day
+
+        expect(should be_expired)
+      end
+    end
+
+    context 'when expiration_date is in the future' do
+      it 'returns false' do
+        coupon.expiration_date = DateTime.now + 1.day
+
+        expect(should_not be_expired)
+      end
+    end
+  end
 end
