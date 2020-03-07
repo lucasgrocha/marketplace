b4195ed051f621f76be24f83ba5a9700e2d92e04
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-03-07 20:21:27 -0300

    endpoint to create new coupon

---
+++ app/controllers/api/v1/coupon_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::CouponController < ApplicationController
+  skip_forgery_protection
+  before_action :check_presence
+
+  rescue_from ActiveRecord::InvalidForeignKey do |e|
+    render json: { error: 'Could not find category' }, status: :not_found
+  end
+
+  def create
+    @coupon = Coupon.new(coupon_params)
+
+    if @coupon.save
+      respond_to do |format|
+        format.json { render json: coupon_params , status: :created }
+      end
+    end
+  end
+
+  private
+
+  def check_presence
+    if Coupon.exists?(coupon_params['code'])
+      render json: { error: 'Coupon already exists' }, status: :unprocessable_entity
+    end
+  end
+
+  def coupon_params
+    params.require(:coupon).permit(:code, :discount_amount, :discount_type, :expiration_date, :category_id)
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  rescue_from ArgumentError do |e|
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
 end
